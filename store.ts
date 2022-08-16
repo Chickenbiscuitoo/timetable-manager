ba638bf17dc7e3747355dbb1d811d8ed8ed84599
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-08-16 10:42:34 +0200

    added addLesson func to store

--- store.ts
+++ store.ts
@@ -1,24 +1,30 @@
 import create from 'zustand'
 
 interface TimetableState {
-	timetableData: {
+	rawTableData: {
 		lesson_id: number
 		subject: string
 		teacher: string
 	}[]
+
+	addLesson: (lessonId: number, subject: string, teacher: string) => void
 }
 
-const useTimetableStore = create<TimetableState>(() => ({
-	timetableData: [
+const useTimetableStore = create<TimetableState>((set) => ({
+	rawTableData: [
 		{
 			lesson_id: 101,
 			subject: 'PCI',
 			teacher: 'RP',
+			day: 'Monday',
+			lesson: '1',
 		},
 		{
 			lesson_id: 102,
 			subject: 'PCI',
 			teacher: 'RP',
+			day: 'Monday',
+			lesson: '2',
 		},
 		{
 			lesson_id: 104,
@@ -54,13 +60,34 @@ const useTimetableStore = create<TimetableState>(() => ({
 			lesson_id: 501,
 			subject: 'PCI',
 			teacher: 'RP',
+			day: 'Friday',
+			lesson: '1',
 		},
 		{
 			lesson_id: 511,
 			subject: 'PCI',
 			teacher: 'RP',
+			day: 'Friday',
+			lesson: '11',
+		},
+		{
+			lesson_id: 308,
+			subject: 'FYZ',
+			teacher: 'PL',
 		},
 	],
+
+	addLesson: (lesson_id, subject, teacher) =>
+		set((state) => ({
+			rawTableData: [
+				...state.rawTableData,
+				{
+					lesson_id,
+					subject,
+					teacher,
+				},
+			],
+		})),
 }))
 
 export default useTimetableStore
