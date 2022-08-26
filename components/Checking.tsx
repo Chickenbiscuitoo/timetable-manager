2dffb832a7c31866cb6f275b86b449e507cf2cb6
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-08-26 11:24:36 +0200

    added validators

--- components/Checking.tsx
+++ components/Checking.tsx
@@ -1,20 +1,18 @@
-// či daný učiteľ neučí menej ako 22 a viac ako jeho maximum
-// či sú pokryté všetky hodiny predmetu učiteľmi
-// či triedny učiteľ učí nejaký predmet vo svojej triede
-// či má trieda priradené všetky hodiny
-
 import type { NextPage } from 'next'
 import styles from '../styles/Checking.module.css'
+import { useState } from 'react'
 
 import { flatten } from '../utils/arraysFuncs'
 
 import useTimetableStore from '../store'
 
 const Checking: NextPage = () => {
+	const [hovered, setHovered] = useState(false)
+
 	const { rawTableData, teachers, selectedClass, classes } =
 		useTimetableStore()
 
-	// array of teachers and count of lessons they teach
+	// returns  an array of teachers and count of lessons they teach
 	const teachersStats = teachers.map((teacher) => {
 		// list of all teachers that are teaching some lessons
 		const activeTeachers = rawTableData.map((lesson) =>
@@ -29,6 +27,22 @@ const Checking: NextPage = () => {
 			id: teacher.id,
 			name: teacher.name,
 			lessons: teacherLessons.length,
+		}
+	})
+
+	const teachersInvalid = teachersStats.map((teacher) => {
+		if (teacher.lessons > 28) {
+			return (
+				<div className={styles.invalid} key={teacher.id}>
+					{teacher.name} - {teacher.lessons}
+				</div>
+			)
+		} else if (teacher.lessons < 22) {
+			return (
+				<div className={styles.warning} key={teacher.id}>
+					{teacher.name} - {teacher.lessons}
+				</div>
+			)
 		}
 	})
 
@@ -58,8 +72,47 @@ const Checking: NextPage = () => {
 		}
 	}
 
-	classTeacherTeaching()
-	return <div>jou</div>
+	const classLessonsNumInvalid = () => {
+		const classLessons = rawTableData.filter(
+			(lesson) => lesson.class === selectedClass
+		)
+
+		if (classLessons.length > 30) {
+			return (
+				<div className={styles.invalid}>
+					{classLessons.length} - too many lessons
+				</div>
+			)
+		} else if (classLessons.length < 20) {
+			return (
+				<div className={styles.warning}>
+					{classLessons.length} - too few lessons
+				</div>
+			)
+		}
+	}
+
+	return (
+		<div>
+			<div
+				onMouseEnter={() => setHovered(true)}
+				onMouseLeave={() => setHovered(false)}
+			>
+				<p>Teachers teaching invalid number of lessons (hover)</p>
+				{hovered && (
+					<div className={styles.teachers_list}>
+						{teachersInvalid}
+					</div>
+				)}
+			</div>
+			{!classTeacherTeaching() && (
+				<p className={styles.invalid}>
+					Class teacher is not teaching any lessons!
+				</p>
+			)}
+			{classLessonsNumInvalid()}
+		</div>
+	)
 }
 
 export default Checking
