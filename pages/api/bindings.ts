8672fd7498643138fa666805da5b5be984c7bb88
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-12-11 11:39:58 +0100

    added GET bindings route

--- pages/api/bindings.ts
+++ pages/api/bindings.ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../server/client'
-import axios from 'axios'
 
 export default async function handler(
 	req: NextApiRequest,
@@ -14,6 +13,9 @@ export default async function handler(
 
 	try {
 		const data = await prisma.binding.findMany({
+			where: {
+				id: 1,
+			},
 			include: {
 				teachers: true,
 				class: true,
@@ -22,7 +24,29 @@ export default async function handler(
 			},
 		})
 
-		return res.status(200).json(data)
+		// map each binding from data and insert number of lessons each teacher teaches from BindingTeacherLessons into teachers object
+		const updatedData = data.map((binding) => {
+			const updatedTeachers = binding.teachers.map((teacher) => {
+				const records = binding.BindingTeacherLessons.find(
+					(entry) => entry.teacherId === teacher.id
+				)
+
+				return {
+					...teacher,
+					lessons: records ? records.lessons : 0,
+				}
+			})
+
+			const updatedBinding = {
+				...binding,
+				teachers: updatedTeachers,
+				BindingTeacherLessons: undefined,
+			}
+
+			return updatedBinding
+		})
+
+		return res.status(200).json(updatedData)
 	} catch (error) {
 		let message = 'Unknown Error'
 
@@ -32,3 +56,34 @@ export default async function handler(
 		return res.status(500).json({ message })
 	}
 }
+
+// [
+// 	{
+// 		id: 1,
+// 		subjectId: 4,
+// 		classId: 1,
+// 		teachers: [
+// 			{
+// 				id: 1,
+// 				name: 'John Doe',
+// 				shortname: 'JD',
+// 				email: 'john.doe@gmail.com',
+// 				lessons: 7,
+// 			},
+// 			{
+// 				id: 2,
+// 				name: 'Francis Muller',
+// 				shortname: 'FM',
+// 				email: 'francis.muller@gmail.com',
+// 				lessons: 4,
+// 			},
+// 		],
+// 		class: { id: 1, name: '1.A', grade: 1, teacherId: 1 },
+// 		subject: {
+// 			id: 4,
+// 			name: 'Physics',
+// 			shortname: 'PHY',
+// 			commiteeId: 3,
+// 		},
+// 	},
+// ]
