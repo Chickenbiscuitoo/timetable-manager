592e202ed49b4dfaeb85121d9acacc830ec4a3c0
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-12-14 16:28:57 +0100

    added delete

--- pages/api/classes.ts
+++ pages/api/classes.ts
@@ -3,10 +3,14 @@ import { Class } from '@prisma/client'
 import { prisma } from '../../server/client'
 import { z } from 'zod'
 
-const schema = z.object({
+const schemaPUT = z.object({
 	name: z.string().min(3).max(128),
 	grade: z.number().int().positive(),
 	teacherId: z.number().int().positive(),
+})
+
+const schemaDELETE = z.object({
+	id: z.number().int().positive(),
 })
 
 type updatedClass = Omit<Class, 'teacherId'>
@@ -42,12 +46,30 @@ export default async function handler(
 		}
 	} else if (method === 'PUT') {
 		try {
-			const data = schema.parse(req.body)
+			const data = schemaPUT.parse(req.body)
 			const response = await prisma.class.create({
 				data: {
 					name: data.name,
 					grade: data.grade,
 					teacherId: data.teacherId,
+				},
+			})
+
+			return res.status(200).json({ message: response })
+		} catch (error) {
+			let message = 'Unknown Error'
+
+			if (error instanceof Error) message = error.message
+			else message = String(error)
+
+			return res.status(500).json({ message })
+		}
+	} else if (method === 'DELETE') {
+		try {
+			const data = schemaDELETE.parse(req.body)
+			const response = await prisma.class.delete({
+				where: {
+					id: data.id,
 				},
 			})
 
