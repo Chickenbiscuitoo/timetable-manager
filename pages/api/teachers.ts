b903aadfb436261ca8af244cd98bdcef0f4b689b
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-12-17 12:14:08 +0100

    added DELETE method

--- pages/api/teachers.ts
+++ pages/api/teachers.ts
@@ -3,10 +3,14 @@ import { Teacher } from '@prisma/client'
 import { prisma } from '../../server/client'
 import { z } from 'zod'
 
-const schema = z.object({
+const schemaPUT = z.object({
 	name: z.string().min(3).max(128),
 	shortname: z.string().min(2).max(8),
 	email: z.string().email(),
+})
+
+const schemaDELETE = z.object({
+	id: z.number().int().positive(),
 })
 
 export default async function handler(
@@ -30,12 +34,30 @@ export default async function handler(
 		}
 	} else if (method === 'PUT') {
 		try {
-			const data = schema.parse(req.body)
+			const data = schemaPUT.parse(req.body)
 			const response = await prisma.teacher.create({
 				data: {
 					name: data.name,
 					shortname: data.shortname,
 					email: data.email,
+				},
+			})
+
+			return res.status(200).json({ message: response })
+		} catch (error) {
+			let message = 'Unknown Error'
+
+			if (error instanceof Error) message = error.message
+			else message = String(error)
+
+			return res.status(500).json({ message })
+		}
+	} else if (method === 'DELETE') {
+		try {
+			const data = schemaDELETE.parse(req.body)
+			const response = await prisma.teacher.delete({
+				where: {
+					id: data.id,
 				},
 			})
 
