36adcc3abe44f41990ee40a8c7ccd421896f98b2
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-12-23 10:55:52 +0100

    added patch method

--- pages/api/classes.ts
+++ pages/api/classes.ts
@@ -13,6 +13,13 @@ const schemaDELETE = z.object({
 	id: z.number().int().positive(),
 })
 
+const schemaPATCH = z.object({
+	id: z.number().int().positive(),
+	name: z.string().min(3).max(128),
+	grade: z.number().int().positive(),
+	teacherId: z.number().int().positive(),
+})
+
 type updatedClass = Omit<Class, 'teacherId'>
 
 export default async function handler(
@@ -70,6 +77,29 @@ export default async function handler(
 			const response = await prisma.class.delete({
 				where: {
 					id: data.id,
+				},
+			})
+
+			return res.status(200).json({ message: response })
+		} catch (error) {
+			let message = 'Unknown Error'
+
+			if (error instanceof Error) message = error.message
+			else message = String(error)
+
+			return res.status(500).json({ message })
+		}
+	} else if (method === 'PATCH') {
+		try {
+			const data = schemaPATCH.parse(req.body)
+			const response = await prisma.class.update({
+				where: {
+					id: data.id,
+				},
+				data: {
+					name: data.name,
+					grade: data.grade,
+					teacherId: data.teacherId,
 				},
 			})
 
