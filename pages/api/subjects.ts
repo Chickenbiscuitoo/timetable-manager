24b404f0b3076429dd01401353e5937e2982d9ef
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-12-23 10:57:25 +0100

    added PATCH method

--- pages/api/subjects.ts
+++ pages/api/subjects.ts
@@ -13,6 +13,13 @@ const schemaDELETE = z.object({
 	id: z.number().int().positive(),
 })
 
+const schemaPATCH = z.object({
+	id: z.number().int().positive(),
+	name: z.string().min(3).max(255),
+	shortname: z.string().min(2).max(8),
+	commiteeId: z.number().int().positive(),
+})
+
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
@@ -58,6 +65,29 @@ export default async function handler(
 			const response = await prisma.subject.delete({
 				where: {
 					id: data.id,
+				},
+			})
+
+			return res.status(200).json({ message: response })
+		} catch (error) {
+			let message = 'Unknown Error'
+
+			if (error instanceof Error) message = error.message
+			else message = String(error)
+
+			return res.status(500).json({ message })
+		}
+	} else if (method === 'PATCH') {
+		try {
+			const data = schemaPATCH.parse(req.body)
+			const response = await prisma.subject.update({
+				where: {
+					id: data.id,
+				},
+				data: {
+					name: data.name,
+					shortname: data.shortname,
+					commiteeId: data.commiteeId,
 				},
 			})
 
