3cf05b2d9ae66de5d24c4b55da22d59425689459
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-12-24 13:28:50 +0100

    styles

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -7,35 +7,31 @@ const LoginButton = () => {
 
 	if (session.status === 'authenticated') {
 		return (
-			<>
-				<button
-					onClick={() =>
-						signOut({
-							callbackUrl: 'http://localhost:3000/landing',
-						})
-					}
-					className="cursor-pointer flex items-center text-base font-normal rounded-lg border border-transparent transition duration-200 hover:border-primary"
-				>
-					<FaSignOutAlt className="w-fit h-full bg-neutral transition duration-75 mr-1" />
-					Sign Out
-				</button>
-			</>
+			<button
+				onClick={() =>
+					signOut({
+						callbackUrl: 'http://localhost:3000/landing',
+					})
+				}
+				className="btn gap-2"
+			>
+				<FaSignOutAlt />
+				Sign Out
+			</button>
 		)
 	}
 	return (
-		<>
-			<button
-				onClick={() =>
-					signIn('google', {
-						callbackUrl: 'http://localhost:3000',
-					})
-				}
-				className="cursor-pointer flex items-center text-base font-normal rounded-lg border border-transparent transition duration-200 hover:border-primary"
-			>
-				<FaSignInAlt className="w-fit h-full bg-neutral transition duration-75 mr-1" />
-				Sign In
-			</button>
-		</>
+		<button
+			onClick={() =>
+				signIn('google', {
+					callbackUrl: 'http://localhost:3000',
+				})
+			}
+			className="btn gap-2"
+		>
+			<FaSignInAlt />
+			Sign In
+		</button>
 	)
 }
 
