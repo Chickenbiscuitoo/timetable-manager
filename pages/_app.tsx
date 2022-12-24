080c233aae5fff8f9b1d9c6e01e9c9a5ad2cd108
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-12-24 11:55:48 +0100

    fixed wrong path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { SessionProvider } from 'next-auth/react'
-import './styles.css'
+import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 import type { Session } from 'next-auth'
