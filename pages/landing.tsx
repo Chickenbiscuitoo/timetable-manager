aea92f1586c1b200f234ed869ed22c8411171f77
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-12-24 12:44:30 +0100

    added landing page

---
+++ pages/landing.tsx
@@ -0,0 +1,47 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+import LoginButton from '../components/LoginButton'
+
+import { AiFillGithub } from 'react-icons/ai'
+
+const Landing: NextPage = () => {
+	const router = useRouter()
+	const { data: session, status } = useSession()
+
+	if (session && status === 'authenticated') {
+		router.push('/')
+	}
+
+	return (
+		<div>
+			<Head>
+				<title>Timetable Manager</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main className="min-h-screen flex flex-row">
+				Landing
+				<LoginButton />
+			</main>
+
+			<footer className="flex place-content-center border-t-2 border-neutral pt-6 pb-6">
+				<a
+					href="https://github.com/Chickenbiscuitoo"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="text-neutral"
+				>
+					Created by Chickenbiscuitoo
+					<span className="ml-1 inline">
+						<AiFillGithub className="inline h-full" />
+					</span>
+				</a>
+			</footer>
+		</div>
+	)
+}
+
+export default Landing
