cbbffb9d29fd77a4e9778a51a9eb499fa18c837b
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-12-25 18:13:42 +0100

    added form

--- components/ProfileCardOrg.tsx
+++ components/ProfileCardOrg.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 
 import useTimetableStore from '../store'
 
 const ProfileCardOrg: NextPage = () => {
 	const { organization } = useTimetableStore()
+
+	const [inviteEmail, setInviteEmail] = useState('')
 
 	return (
 		<div className="bg-base-100 rounded-xl p-6 mt-2">
@@ -24,6 +27,8 @@ const ProfileCardOrg: NextPage = () => {
 					<input
 						type="text"
 						placeholder="email@gmail.com"
+						value={inviteEmail}
+						onChange={(e) => setInviteEmail(e.target.value)}
 						className="input input-bordered input-primary w-8/12 max-w-xs mr-2"
 					/>
 					<button className="btn btn-outline btn-success w-3/12 min-h-full">
