b845aaab557e1b2233efe7d2dc546a1216f03d58
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-12-26 18:18:22 +0100

    added commitee endpoint

---
+++ pages/api/commitees.ts
@@ -0,0 +1,109 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getServerAuthSession } from '../../server/common/get-server-auth-session'
+
+import { Committee } from '@prisma/client'
+import { prisma } from '../../server/client'
+import { z } from 'zod'
+
+import cookie from 'cookie'
+
+const schemaPUT = z.object({
+	name: z.string().min(3).max(128),
+	chairmanId: z.number().int().positive(),
+})
+
+const schemaDELETE = z.object({
+	id: z.number().int().positive(),
+})
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const session = await getServerAuthSession({ req, res })
+
+	if (!session) {
+		return res.status(403).send({
+			error: 'You must be signed in to view the protected content on this page.',
+		})
+	}
+
+	const cookies = cookie.parse(req.headers.cookie || '')
+	const token =
+		process.env.NODE_ENV == 'development'
+			? cookies['next-auth.session-token']
+			: cookies['__Secure-next-auth.session-token']
+
+	const userSession = await prisma.session.findUnique({
+		where: { sessionToken: token },
+	})
+
+	if (!userSession) {
+		return res.status(403).send({
+			error: 'You must be signed in to view the protected content on this page.',
+		})
+	}
+
+	const { method } = req
+
+	if (method === 'GET') {
+		try {
+			const data = await prisma.committee.findMany({
+				where: {
+					ownerId: userSession.userId,
+				},
+			})
+
+			return res.status(200).json(data)
+		} catch (error) {
+			let message = 'Unknown Error'
+
+			if (error instanceof Error) message = error.message
+			else message = String(error)
+
+			return res.status(500).json({ message })
+		}
+	} else if (method === 'PUT') {
+		try {
+			const data = schemaPUT.parse(req.body)
+			const response = await prisma.committee.create({
+				data: {
+					name: data.name,
+					chairmanId: data.chairmanId,
+					ownerId: userSession.userId,
+				},
+			})
+
+			return res.status(200).json({ message: response })
+		} catch (error) {
+			let message = 'Unknown Error'
+
+			if (error instanceof Error) message = error.message
+			else message = String(error)
+
+			return res.status(500).json({ message })
+		}
+	} else if (method === 'DELETE') {
+		try {
+			const data = schemaDELETE.parse(req.body)
+			const response = await prisma.committee.delete({
+				where: {
+					id: data.id,
+				},
+			})
+
+			return res.status(200).json({ message: response })
+		} catch (error) {
+			let message = 'Unknown Error'
+
+			if (error instanceof Error) message = error.message
+			else message = String(error)
+
+			return res.status(500).json({ message })
+		}
+	} else {
+		return res.status(400).json({
+			message: 'Only GET, PUT and DELETE method allowed',
+		})
+	}
+}
