01b511f2636d253ec696e92e5f9ea13b189ae61c
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2023-01-02 13:15:31 +0100

    added binding table

--- components/BindingCard.tsx
+++ components/BindingCard.tsx
@@ -6,6 +6,14 @@ interface BindingCardProps {
 	teacherId: number
 }
 
+interface BindingTeacher {
+	id: number
+	name: string
+	shortname: string
+	email: string
+	lessons: number
+}
+
 const BindingCard: NextPage<BindingCardProps> = ({ teacherId }) => {
 	const { teachers, bindings } = useTimetableStore()
 
@@ -17,10 +25,49 @@ const BindingCard: NextPage<BindingCardProps> = ({ teacherId }) => {
 		)
 		.map((bind) => ({
 			...bind,
-			teacher: bind.teachers.find((tch) => tch.id === teacherId),
+			teacher: bind.teachers.find(
+				(tch) => tch.id === teacherId
+			) as BindingTeacher,
 		}))
 
-	return <div className="absolute">BindingCardos</div>
+	const teacherTotalLessons = teacherBindings.reduce((acc, binding) => {
+		return acc + binding.teacher.lessons
+	}, 0)
+
+	return (
+		<div
+			tabIndex={0}
+			className="fixed right-56 z-50 card compact dropdown-content shadow bg-base-100 rounded-box w-fit-content h-fit-content"
+		>
+			<div className="card-compact bg-base-300 border border-transparent rounded-xl shadow-lg">
+				<table className="card-body ">
+					<tbody>
+						<tr>
+							<td className="border border-white text-white text-lg p-3 font-bold">
+								{teacher?.shortname}
+							</td>
+							<td className="border border-white text-white text-lg p-3 font-bold">
+								{teacherTotalLessons}h
+							</td>
+							{teacherBindings.map((binding) => (
+								<td className="border border-white text-white p-3 text-center">
+									<h3 className="text-md font-semibold">
+										{binding.cl.name}
+									</h3>
+									<h3 className="text-md font-bold">
+										{binding.subject.shortname}
+									</h3>
+									<h3 className="text-sm">
+										{binding.teacher.lessons}h
+									</h3>
+								</td>
+							))}
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	)
 }
 
 export default BindingCard
