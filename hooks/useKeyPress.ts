48735855be22a307711a8ec24c41034ebde18632
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2023-03-27 19:33:49 +0200

    updated shortcuts hook

--- hooks/useKeyPress.ts
+++ hooks/useKeyPress.ts
@@ -5,16 +5,13 @@ const useKeyPress = (
 	callback: (event: KeyboardEvent) => void,
 	node: HTMLElement | null = null
 ) => {
-	// implement the callback ref pattern
 	const callbackRef = useRef(callback)
 	useLayoutEffect(() => {
 		callbackRef.current = callback
 	})
 
-	// handle what happens on key press
 	const handleKeyPress = useCallback(
 		(event: KeyboardEvent) => {
-			// check if one of the key is part of the ones we want
 			if (event.ctrlKey && key === event.key) {
 				callbackRef.current(event)
 			}
@@ -23,16 +20,13 @@ const useKeyPress = (
 	)
 
 	useEffect(() => {
-		// target is either the provided node or the document
 		const targetNode = node ?? document
-		// attach the event listener
 		targetNode &&
 			targetNode.addEventListener(
 				'keydown',
 				handleKeyPress as EventListener
 			)
 
-		// remove the event listener
 		return () =>
 			targetNode &&
 			targetNode.removeEventListener(
